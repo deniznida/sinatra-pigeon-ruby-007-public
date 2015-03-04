526fc96aaf04571ca32daa01dba04f9b394e5d28
deniznida <denizunat@hotmail.com>
2015-03-04 17:28:46 -0500

    done

--- app/controllers/pigeons_controller.rb
+++ app/controllers/pigeons_controller.rb
@@ -1,7 +1,45 @@
 class PigeonsController < ApplicationController
 
   get '/pigeons' do 
-    # your code here
+    @pigeons = Pigeon.all
+    erb :'pigeons/index'
   end
+
+  post '/pigeons' do
+    @pigeon = Pigeon.new
+    @pigeon.name = params["name"]
+    @pigeon.gender = params["gender"]
+    @pigeon.color = params["color"]
+    @pigeon.lives = params["lives"]
+    @pigeon.save
+    redirect '/pigeons'
+  end
+
+  get '/pigeons/new' do
+    erb :'pigeons/new'
+  end
+
+  get '/pigeons/:id/edit' do
+    @pigeon = Pigeon.find(params[:id])
+    erb :'pigeons/show'
+  end
+
+  get '/pigeons/:id' do
+    @pigeon = Pigeon.find(params[:id])
+    erb :'pigeons/show'
+  end
+
+  patch '/pigeons/:id/update' do
+    @pigeon = Pigeon.find(params[:id])
+    @pigeon.update(name: params["name"], gender: params["gender"], color: params["color"], lives: params["lives"])
+    redirect '/pigeons'
+  end
+
+  post '/pigeons/:id/destroy' do
+    @pigeon = Pigeon.find(params[:id])
+    @pigeon.destroy
+    redirect '/pigeons'
+  end
+  
 
 end
